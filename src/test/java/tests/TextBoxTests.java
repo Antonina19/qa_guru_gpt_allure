7c6552ba24d55e70ac05cc000c090bcffb2a7518
totosha <tony-94@bk.ru>
2024-03-13 16:42:25 +0300

    added Attach.java, TestBase.java, TextBoxTests.java

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class TextBoxTests extends TestBase {
+
+    @Test
+    void fillFormTest() {
+        step("Open page /text-box", () -> {
+            open("/text-box");
+
+            if($(".fc-dialog-content").isDisplayed())
+                $(byText("Consent")).click();
+        });
+        step("Fill form", () -> {
+            $("#userName").setValue("Alex");
+            $("#userEmail").setValue("alex@egorov.com");
+            $("#currentAddress").setValue("Some street 1");
+            $("#permanentAddress").setValue("Another street 1");
+            $("#submit").click();
+        });
+        step("Check results", () -> {
+            $("#output #name").shouldHave(text("Alex"));
+            $("#output #email").shouldHave(text("alex@egorov.com"));
+            $("#output #currentAddress").shouldHave(text("Some street 1"));
+            $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        });
+    }
+}
