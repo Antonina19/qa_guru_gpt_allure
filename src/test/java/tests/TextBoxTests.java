117e46217b23222d6494a9c7b87d884c3c39bbaf
totosha <tony-94@bk.ru>
2024-03-13 17:06:47 +0300

    update build.gradle and added tests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TextBoxTests extends TestBase {
 
@@ -34,6 +34,17 @@ public class TextBoxTests extends TestBase {
             $("#output #email").shouldHave(text("alex@egorov.com"));
             $("#output #currentAddress").shouldHave(text("Some street 1"));
             $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        });
+    }
+    @Test
+    void titleTest() {
+        step("Open page /text-box", () -> {
+            open("/text-box");
+        });
+        step("Title of page equals DEMOQA", () -> {
+            String expectedTitle = "DEMOQA";
+            String actualTitle = title();
+            assertThat(actualTitle).isEqualTo(expectedTitle);
         });
     }
 }
